c7d56ff5bbfc56507e2cc21dab643bd7baa3747d
Baris Can Akkocak <bariscanakkocak@gmail.com>
2022-12-18 20:26:14 +0300

    Improve layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { DetailsComponent } from './pages/details/details.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: HomepageComponent,
+  },
+  {
+    path: 'details',
+    component: DetailsComponent,
+  },
+];
+
 @NgModule({
   declarations: [AppComponent, HomepageComponent, DetailsComponent],
-  imports: [BrowserModule, TranslateModule.forRoot()],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    TranslateModule.forRoot(),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
