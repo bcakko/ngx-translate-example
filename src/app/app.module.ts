7623326d8da5e345cdbfdbff5ff41e7d5b7461c1
Baris Can Akkocak <bariscanakkocak@gmail.com>
2022-12-18 19:56:24 +0300

    Create general structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { DetailsComponent } from './pages/details/details.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomepageComponent, DetailsComponent],
   imports: [BrowserModule, TranslateModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
